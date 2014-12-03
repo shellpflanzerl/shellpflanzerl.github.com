94f99fa2577bc9999af4ca5c52fd856a8c19dc55
Georg Vogetseder <georg.vogetseder@gmail.com>
2014-12-03 20:44:18 +0100

    Initial Commit

---
+++ content/blog/Misc/hello_world.md
@@ -0,0 +1,9 @@
+Title: Hello World
+Date: 2014-12-03 21:00
+Tags: test
+Authors: Georg Vogetseder
+Summary: This is a test of the emergency broadcast system. Please stand by.
+
+This is a test of the **emergency broadcast system**. Please stand by.
+
+![Yes, master.]({filename}/images/mad-dog.jpg)
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'The Shellpflanzerl Collective'
+SITENAME = 'shellpflanzerl'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Vienna'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+STATIC_PATHS = ['images']
+ARTICLE_PATHS = ['blog']
